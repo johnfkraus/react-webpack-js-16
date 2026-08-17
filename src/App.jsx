9be575c31b6dc0a71b003ce7048ba55c4475cb6b
John Kraus <johnkraus3@gmail.com>
2026-08-17 17:54:13 -0400

    added vite, removed webpack, babel

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-// import React from 'react';
-
+// src/App.jsx
 function App() {
     return (
         <div style={{ fontFamily: 'sans-serif', padding: '2rem' }}>
             <h1>Hello, React 19!</h1>
-            <p>This app is bundled with Webpack 5 and Babel.</p>
+            <p>This app is bundled with Vite.</p>
         </div>
     );
 }
