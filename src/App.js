c9583e09b23532e34103576c3eeffe526625cffb
John Kraus <johnkraus3@gmail.com>
2026-08-17 17:39:25 -0400

    migrated from 16 to 17

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
+// import React from 'react';
 
 function App() {
     return (
         <div style={{ fontFamily: 'sans-serif', padding: '2rem' }}>
-            <h1>Hello, React 16!</h1>
+            <h1>Hello, React 17!</h1>
             <p>This app is bundled with Webpack 5 and Babel.</p>
         </div>
     );
