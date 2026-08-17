34a1f682b6fe48d1ae600d99e701f3e1963bb333
John Kraus <johnkraus3@gmail.com>
2026-08-17 17:31:34 -0400

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div style={{ fontFamily: 'sans-serif', padding: '2rem' }}>
+            <h1>Hello, React 16!</h1>
+            <p>This app is bundled with Webpack 5 and Babel.</p>
+        </div>
+    );
+}
+
+export default App;
